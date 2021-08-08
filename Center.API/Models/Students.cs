c1fcdd79e5bd2e0ede088296277802a17ac3d99d
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-08 19:53:50 +0500

    add Student class

---
+++ Center.API/Models/Students.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Models
+{
+    public class Students
+    {
+
+        public int Id { get; set; }
+        public string  FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string  Phone { get; set; }
+
+    }
+}
