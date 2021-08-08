534bb4f548a6a18aeb67672fe94f53eadf502cd2
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-08 20:01:37 +0500

    add other property

--- Center.API/Models/Students.cs
+++ Center.API/Models/Students.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,24 @@ namespace Center.API.Models
     public class Students
     {
 
+        [Key]
+
         public int Id { get; set; }
+
+
+        [Required(ErrorMessage ="FirstName is Required")]
+        [MaxLength(20, ErrorMessage ="FirstName length very long")]
         public string  FirstName { get; set; }
+
+
+
+        [Required(ErrorMessage = "LastName is Required")]
+        [MaxLength(20, ErrorMessage = "LastName length very long")]
         public string  LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Phone is Required")]
+        [MaxLength(20, ErrorMessage = "Phone length very long")]
         public string  Phone { get; set; }
 
     }
