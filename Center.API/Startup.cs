4cd749371b28b2bb1eaf757061d9d6b2c1bc00f9
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-19 18:45:53 +0500

    add service dbcontext

--- Center.API/Startup.cs
+++ Center.API/Startup.cs
@@ -1,7 +1,9 @@
+using Center.API.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,9 @@ namespace Center.API
         {
 
             services.AddControllers();
+
+            services.AddDbContext<CenterContext>(options => options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Center.API", Version = "v1" });
