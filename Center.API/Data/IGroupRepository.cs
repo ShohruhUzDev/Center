95eb608e4e39a63f4f4a0b127001830dcdf9d8fa
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-19 20:57:15 +0500

    add methods to interfase

--- Center.API/Data/IGroupRepository.cs
+++ Center.API/Data/IGroupRepository.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using Center.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Threading.Tasks;
 
 namespace Center.API.Data
 {
-    interface IGroupRepository
+  public   interface IGroupRepository
     {
+        Task<IEnumerable<Group>> GetAllGroupsAsync();
+        Task<Group> GetbyIdGroupAsync(int id);
+        Task CreateGroupAsync(Group group);
+        Task UpdateGroupAsync(Group group);
+        Task DeleteGroupAsync(int id);
+        Task<bool> ExistGroup(int id);
+
+
     }
 }
