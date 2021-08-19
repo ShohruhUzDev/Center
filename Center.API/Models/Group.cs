ee68c7f35def58a673df1ec6c954ae5915cf087e
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-19 18:44:23 +0500

    add Group class

---
+++ Center.API/Models/Group.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Center.API.Models
+{
+    public class Group
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "GroupName is Required")]
+        [MaxLength(20, ErrorMessage = "GroupName length is very long")]
+        public string GroupName { get; set; }
+
+       
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+
+
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+
+        public List<Student> Students { get; set; } = new List<Student>();
+
+
+    }
+
+}
