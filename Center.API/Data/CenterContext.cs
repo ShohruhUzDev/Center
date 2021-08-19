67e8fcfe6c4947d806ecfff20d09893ed3bb0ad2
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-19 18:43:29 +0500

    for student class add repository

---
+++ Center.API/Data/CenterContext.cs
@@ -0,0 +1,21 @@
+﻿using Center.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Data
+{
+    public class CenterContext:DbContext
+    {
+        public CenterContext(DbContextOptions<CenterContext> options):base(options)
+        {
+
+        }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+    }
+}
