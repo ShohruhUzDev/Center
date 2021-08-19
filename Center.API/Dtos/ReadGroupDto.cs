d55b0d399900f96d3b5df3f9062f6b79d9bc0394
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-19 21:05:28 +0500

    add read dto class

---
+++ Center.API/Dtos/ReadGroupDto.cs
@@ -0,0 +1,27 @@
+﻿using Center.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Dtos
+{
+    public class ReadGroupDto
+    {
+       
+        public int Id { get; set; }
+
+
+       
+        public string GroupName { get; set; }
+
+
+        public int TeacherId { get; set; }
+      
+
+        public int SubjectId { get; set; }
+     
+
+        public List<Student> Students { get; set; } = new List<Student>();
+    }
+}
