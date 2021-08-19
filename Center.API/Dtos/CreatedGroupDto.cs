d5a82a040c819d0db5abd726d276b1c75819fb7d
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-19 21:05:16 +0500

    add create dto class

---
+++ Center.API/Dtos/CreatedGroupDto.cs
@@ -0,0 +1,32 @@
+﻿using Center.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Dtos
+{
+    public class CreatedGroupDto
+    {
+        [Key]
+        [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+
+        [Required(ErrorMessage = "GroupName is Required")]
+        [MaxLength(20, ErrorMessage = "GroupName length is very long")]
+        public string GroupName { get; set; }
+
+
+        public int TeacherId { get; set; }
+       
+
+        public int SubjectId { get; set; }
+        
+
+        
+    }
+}
