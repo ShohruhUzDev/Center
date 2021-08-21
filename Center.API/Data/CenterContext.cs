c93febb77f0a5b8cc997066071da28003d27fea6
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:28:34 +0500

    add GroupStudent dbset

--- Center.API/Data/CenterContext.cs
+++ Center.API/Data/CenterContext.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Center.API.Data
 {
-    public class CenterContext:DbContext
+    public class CenterContext : DbContext
     {
         public CenterContext(DbContextOptions<CenterContext> options):base(options)
         {
@@ -17,5 +17,18 @@ namespace Center.API.Data
         public DbSet<Teacher> Teachers { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Subject> Subjects { get; set; }
+        public DbSet<StudentGroup> StudentGroups { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<StudentGroup>()
+                .HasOne(i => i.Student)
+                .WithMany(u => u.StudentGroups)
+                .HasForeignKey(n => n.StudentId);
+            modelBuilder.Entity<StudentGroup>()
+               .HasOne(i => i.Group)
+               .WithMany(u => u.StudentGroups)
+               .HasForeignKey(n => n.GroupId);
+        }
     }
 }
