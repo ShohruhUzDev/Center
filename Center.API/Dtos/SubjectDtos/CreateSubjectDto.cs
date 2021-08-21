195cc8c8b7c412f631e6df29f27bcbe4334184c6
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:32:34 +0500

    add CreateSubjectDto

---
+++ Center.API/Dtos/SubjectDtos/CreateSubjectDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Dtos.SubjectDtos
+{
+    public class CreateSubjectDto
+    {
+       
+
+
+        [Required(ErrorMessage = "SubjectName is Required")]
+        [MaxLength(20, ErrorMessage = "SubjectName length very long")]
+        public string SubjectName { get; set; }
+
+
+
+        [Required(ErrorMessage = "Price is Required")]
+
+        public int Price { get; set; }
+
+
+    }
+}
