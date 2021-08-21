ac3d4488d213d2c0a5c774a2e7a87d440d085504
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:37:13 +0500

    add StudentGroup class

---
+++ Center.API/Models/StudentGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Models
+{
+    public class StudentGroup
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+        public int GroupId { get; set; }
+        public Group Group { get; set; }
+    }
+}
