bb196c648e3b463647169562a24aa095e85fa508
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:36:51 +0500

    add CenterProfile for AutoMapper

---
+++ Center.API/Profiles/CenterProfile.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Center.API.Dtos;
+using Center.API.Dtos.SubjectDtos;
+using Center.API.Dtos.TeacherDtos;
+using Center.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Threading.Tasks;
+
+namespace Center.API.Profiles
+{
+    public class CenterProfile:Profile
+    {
+        public CenterProfile()
+        {
+            CreateMap<CreatedGroupDto, Group>();
+            CreateMap< Group, ReadGroupDto>();
+            CreateMap<UpdateGroupDto, Group>();
+
+
+            CreateMap<CreateStudentDto, Student>();
+            CreateMap<Student, ReadStudentDto>();
+            CreateMap<UpdateStudentDto, Student>();
+
+
+            CreateMap<Subject, ReadSubjectDto>();
+            CreateMap<CreateSubjectDto, Subject>();
+            CreateMap<UpdateSubjectDto, Subject>();
+
+
+            CreateMap<Teacher, ReadTeacherDto>();
+            CreateMap<CreateTeacherDto, Teacher>();
+            CreateMap<UpdateTeacherDto, Teacher>();
+
+        }
+    }
+}
