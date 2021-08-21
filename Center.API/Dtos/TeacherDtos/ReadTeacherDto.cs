6a995492b0847c85b2cca3ac1f6d74e341309262
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:33:45 +0500

    add readTeacherDto

---
+++ Center.API/Dtos/TeacherDtos/ReadTeacherDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Dtos.TeacherDtos
+{
+    public class ReadTeacherDto
+    {
+      
+        public int Id { get; set; }
+
+
+      
+        public string FirstName { get; set; }
+
+
+
+     
+        public string LastName { get; set; }
+
+
+        public string Phone { get; set; }
+
+
+       
+    }
+}
