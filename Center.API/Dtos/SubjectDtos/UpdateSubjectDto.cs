a3bb283d6ac0929d1dbaecaec91b792d55627044
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:33:21 +0500

    add UpdateSubjectDto

---
+++ Center.API/Dtos/SubjectDtos/UpdateSubjectDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Dtos.SubjectDtos
+{
+    public class UpdateSubjectDto
+    {
+        public int Id { get; set; }
+
+        public string SubjectName { get; set; }
+
+        public int Price { get; set; }
+    }
+}
