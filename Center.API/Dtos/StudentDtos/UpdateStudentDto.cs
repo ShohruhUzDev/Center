eb33f7377a678621bf44a633dd329d4e93e214e4
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:32:20 +0500

    add UpdateStudentDto

---
+++ Center.API/Dtos/StudentDtos/UpdateStudentDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Dtos
+{
+    public class UpdateStudentDto
+    {
+        public int Id { get; set; }
+
+
+
+        public string FirstName { get; set; }
+
+
+
+        public string LastName { get; set; }
+
+
+
+        public string Phone { get; set; }
+
+    }
+}
