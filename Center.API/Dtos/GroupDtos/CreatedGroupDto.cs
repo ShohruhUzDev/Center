24cd831e63aaf1dfc8b54bb2a61eb1331a40ac0b
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:31:18 +0500

    add CreatedGroupDto

---
+++ Center.API/Dtos/GroupDtos/CreatedGroupDto.cs
@@ -0,0 +1,28 @@
+﻿using Center.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Dtos
+{
+    public class CreatedGroupDto
+    {
+       
+
+        [Required(ErrorMessage = "GroupName is Required")]
+        [MaxLength(20, ErrorMessage = "GroupName length is very long")]
+        public string GroupName { get; set; }
+
+
+        public int TeacherId { get; set; }
+       
+
+        public int SubjectId { get; set; }
+        
+
+        
+    }
+}
