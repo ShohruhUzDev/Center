214b17606864a79bdcdf9a34df47ea8755d6ae38
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:32:47 +0500

    add ReadSubjectDto

---
+++ Center.API/Dtos/SubjectDtos/ReadSubjectDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Dtos.SubjectDtos
+{
+    public class ReadSubjectDto
+    {
+        public int Id { get; set; }
+
+        public string SubjectName { get; set; }
+
+        public int Price { get; set; }
+
+
+    }
+}
