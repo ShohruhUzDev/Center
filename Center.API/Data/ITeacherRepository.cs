4f25383f37814751f9daa4d1e499e5679888ded7
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:30:06 +0500

    Add ISubjectRepository

--- Center.API/Data/ITeacherRepository.cs
+++ Center.API/Data/ITeacherRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Center.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,13 @@ namespace Center.API.Data
 {
     public interface ITeacherRepository
     {
+        Task<IEnumerable<Teacher>> GetAllTeachersAsync();
+        Task<Teacher> GetbyIdTeacherAsync(int id);
+        Task CreateTeacherAsync(Teacher teacher);
+        Task UpdateTeacherAsync(Teacher teacher);
+        Task DeleteTeacher(int id);
+        bool ExistTeacher(int id);
+        Task<IEnumerable<Student>> GetAllStudentsByTeacherId(int teacherId);
+        Task<IEnumerable<Group>> GetAsllGroupsByTeacherId(int teacherId);
     }
 }
