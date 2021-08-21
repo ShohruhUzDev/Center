300392ba84bde90b66e1e3756fa49e696fc91b7d
ShohruhUzDev <shohruh7464784@mail.ru>
2021-08-21 23:29:31 +0500

    add IStudentRepository

--- Center.API/Data/IStudentRepository.cs
+++ Center.API/Data/IStudentRepository.cs
@@ -1,11 +1,19 @@
-﻿using System;
+﻿using Center.API.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Center.API.Data
 {
-    interface IStudentRepository
+   public interface IStudentRepository
     {
+        Task<IEnumerable<Student>> GetAllStudentsAsync();
+        Task<Student> GetbyIdStudentAsync(int id);
+        Task CreateStudentAsync(Student group);
+        Task UpdateStudentAsync(Student group);
+        Task DeleteStudent(int id);
+        bool ExistStudent(int id);
+
     }
 }
