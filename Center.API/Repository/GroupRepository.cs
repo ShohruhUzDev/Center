74727e62186270e7c2434d07f9cf2894681b9501
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-02 11:46:25 +0500

    add new Repositories

---
+++ Center.API/Repository/GroupRepository.cs
@@ -0,0 +1,93 @@
+﻿using Center.API.Dtos;
+using Center.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Data
+{
+    public class GroupRepository : IGroupRepository
+    {
+        private readonly CenterContext _centercontext;
+
+        public GroupRepository(CenterContext centerContext)
+        {
+            _centercontext = centerContext;
+        }
+
+       
+
+        public async Task CreateGroupAsync(Group group)
+        {
+            await  _centercontext.Groups.AddAsync(group);
+            await _centercontext.SaveChangesAsync();
+        }
+
+        public async Task DeleteGroup(int id)
+        {
+           
+
+            if (ExistGroup(id))
+            {
+                var deletegroup = _centercontext.Groups.FirstOrDefaultAsync(i => i.Id == id);
+                if(deletegroup!=null)
+                {
+                    _centercontext.Groups.Remove(await deletegroup);
+                    _centercontext.SaveChanges();
+                }
+              
+
+            }
+
+        }
+
+        public  bool ExistGroup(int id)
+        {
+            return  _centercontext.Groups.Any(e => e.Id == id);
+        }
+
+        public async Task<IEnumerable<Group>> GetAllGroupsAsync()
+        {
+            return await _centercontext.Groups.ToListAsync();
+        }
+
+
+        //ishlamayapdi hali
+        //public  async Task<IEnumerable<Student>> GetAllStudentsByGroupId(int groupId)
+        //{
+        //     var result1=await _centercontext.Groups.Include(u => u.Students).FirstOrDefaultAsync(i=>i.Id==groupId);
+           
+        //    return result1.Students;
+        //}
+
+        public async Task<Group> GetbyIdGroupAsync(int id)
+        {
+           
+           return await _centercontext.Groups.FirstOrDefaultAsync(i => i.Id == id);
+      
+        }
+
+
+
+     
+
+        public async Task<IEnumerable<Group>> GetGroupsBySubjectId(int subjectId)
+        {
+            return await _centercontext.Groups.Where(i => i.Subject.Id == subjectId).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Group>> GetGroupsByTeacherId(int teacherId)
+        {
+            return await _centercontext.Groups.Where(i => i.Teacher.Id == teacherId).ToListAsync();
+        }
+
+        public async Task UpdateGroupAsync(Group group)
+        {
+
+            _centercontext.Entry(group).State = EntityState.Modified;
+           await _centercontext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Center.API/Repository/SubjectRepository.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Center.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Data
+{
+    public class SubjectRepository : ISubjectRepository
+    {
+        private readonly CenterContext _centerContext;
+
+        public SubjectRepository(CenterContext centerContext)
+        {
+            _centerContext = centerContext;
+        }
+        public async Task CreateSubjectAsync(Subject subject)
+        {
+            await _centerContext.Subjects.AddAsync(subject);
+            await _centerContext.SaveChangesAsync();
+              
+        }
+
+        public async Task DeleteSubject(int id)
+        {
+            if(ExistSubject(id))
+            {
+                var deletesubject = _centerContext.Subjects.FirstOrDefaultAsync(i => i.Id == id);
+                _centerContext.Subjects.Remove( await deletesubject);
+                await   _centerContext.SaveChangesAsync();
+            }
+            
+
+        }
+
+        public  bool ExistSubject(int id)
+        {
+            return  _centerContext.Subjects.Any(i => i.Id == id);
+        }
+
+        public async Task<IEnumerable<Subject>> GetAllSubjectsAsync()
+        {
+            return await _centerContext.Subjects.ToListAsync();
+        }
+
+        public async Task<Subject> GetbyIdSubjectAsync(int id)
+        {
+          
+            return await _centerContext.Subjects.FirstOrDefaultAsync(i => i.Id == id);
+       
+        }
+
+        public async Task UpdateSubjectAsync(Subject subject)
+        {
+            _centerContext.Entry(subject).State = EntityState.Modified;
+           await _centerContext.SaveChangesAsync();
+        }
+    }
+}
