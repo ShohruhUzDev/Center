8c712075cafa4f1d982d4034942dfe17f7294fba
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-03 11:58:00 +0500

    add views

--- Center.Desktop/View/MainPage.xaml.cs
+++ Center.Desktop/View/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Center.Desktop.Pages.UserControls;
+using Center.Desktop.UserControls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -49,6 +51,32 @@ namespace Center.Desktop.Pages
         private void close_btn_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+       
+        private void Guruhlar_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Guruhlar guruhlar = new Guruhlar();
+            asosiy_oyna_grid.Children.Add(guruhlar);
+        }
+
+        private void Oqituvchilar_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Teachers teachers = new Teachers();
+            asosiy_oyna_grid.Children.Add(teachers);
+        }
+
+        private void Oquvchilar_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Students students = new Students();
+            asosiy_oyna_grid.Children.Add(students);
+        }
+
+        private void Fanlar_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Subjects fanlar = new Subjects();
+            asosiy_oyna_grid.Children.Add(fanlar);
+
         }
     }
 }
