7a93b0d124f8a00dfce4fb98105ae511758db36e
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-04 19:48:06 +0500

    add put method

--- Center.API/Controllers/TeachersController.cs
+++ Center.API/Controllers/TeachersController.cs
@@ -55,41 +55,42 @@ namespace Center.API.Controllers
 
         // PUT: api/Teachers/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutTeacher(Guid id, Teacher teacher)
-        //{
-        //    if (id != teacher.Id)
-        //    {
-        //        return BadRequest();
-        //    }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTeacher(Guid id,[FromBody] CustomTeacherDto teacher)
+        {
+            if (id != teacher.Id)
+            {
+                return BadRequest();
+            }
 
-        //    _context.Entry(teacher).State = EntityState.Modified;
+           
 
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!TeacherExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
+            try
+            {
+                await _teacherRepository.UpdateTeacherAsync(_mapper.Map<Teacher>(teacher));
+               
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_teacherRepository.ExistTeacher(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-        //    return NoContent();
-        //}
+            return NoContent();
+        }
 
         // POST: api/Teachers
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult> PostTeacher([FromBody] TeacherForCreationDto teacher)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return BadRequest();
             }
