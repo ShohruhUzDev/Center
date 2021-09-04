e740458aae01b5cfa7129e18c6284ce8a742cd17
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-04 19:48:17 +0500

    add new changes

--- Center.API/Dtos/GroupDto.cs
+++ Center.API/Dtos/GroupDto.cs
@@ -29,4 +29,19 @@ namespace Center.API.Dtos
     {
         public IList<Guid> Ids { get; set; }
     }
+    public class UpdateGroupDto
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "GroupName is Required")]
+        [MaxLength(20, ErrorMessage = "GroupName length is very long")]
+        public string GroupName { get; set; }
+
+
+        public Guid TeacherId { get; set; }
+
+
+        public Guid SubjectId { get; set; }
+
+    }
 }
