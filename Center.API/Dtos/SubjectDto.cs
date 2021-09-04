456ce1cbe68e80078dd251bbf853c577226b1e81
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-04 19:48:28 +0500

    add new changes

--- Center.API/Dtos/SubjectDto.cs
+++ Center.API/Dtos/SubjectDto.cs
@@ -8,6 +8,7 @@ namespace Center.API.Dtos
 {
     public class CustomSubjectDto
     {
+        public Guid Id { get; set; }
 
         [Required(ErrorMessage = "SubjectName is Required")]
         [MaxLength(20, ErrorMessage = "SubjectName length very long")]
@@ -21,13 +22,22 @@ namespace Center.API.Dtos
     }
     public class SubjectDto : CustomSubjectDto
     {
-        public Guid Id { get; set; }
+       
         public ICollection<GroupDto> Guruhlar { get; set; }
       
     }
-    public class SubjectForCreationDto:CustomSubjectDto
+    public class SubjectForCreationDto
     {
-      
+        [Required(ErrorMessage = "SubjectName is Required")]
+        [MaxLength(20, ErrorMessage = "SubjectName length very long")]
+        public string SubjectName { get; set; }
+
+
+
+        [Required(ErrorMessage = "Price is Required")]
+
+        public int Price { get; set; }
+
     }
 
 }
