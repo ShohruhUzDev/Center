094cc20df542d8a519c0e006dc3027162c80a545
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-05 11:00:59 +0500

    Add Teacher APi adress

---
+++ Center.Desktop/ServiceLayer/API.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ServiceLayer
+{
+   public  class TeacherAPI
+    {
+        public static string BASE_URL = "https://localhost:44314/api/";
+        public static string GetAll_URL =  BASE_URL+"Teachers/GetTeachers";
+        public static string Get_URL =BASE_URL+ "Teacher/Get/";
+        public static string Post_URL =BASE_URL+ "Teacher/PostTeacher";
+        public static string Delete_URL =BASE_URL+ "Teacher/DeleteTeacher/";
+        public static string Put_URL =BASE_URL+ "Teacher/PutTeacher/";
+
+
+    }
+}
