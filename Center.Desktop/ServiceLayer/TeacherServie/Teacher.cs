4521c15bb1ae32a8de34abfa4cedf44049e699b2
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-05 11:00:45 +0500

    add Teacher model to Desktop

---
+++ Center.Desktop/ServiceLayer/TeacherServie/Teacher.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ServiceLayer.TeacherServie
+{
+   public class Teacher
+    {
+      
+        [JsonProperty("Id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("FirstName")]
+        public string FirstName { get; set; }
+
+
+
+        [JsonProperty("LastName")]
+
+        public string LastName { get; set; }
+
+
+        [JsonProperty("Phone")]
+
+        public string Phone { get; set; }
+
+
+        //public virtual ICollection<Group> Groups { get; set; }
+        //public Teacher()
+        //{
+        //    Groups = new List<Group>();
+        //}
+    }
+}
