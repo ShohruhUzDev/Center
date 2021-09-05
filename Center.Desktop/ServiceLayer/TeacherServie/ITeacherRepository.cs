d0100c866ff78e56e6420d5b449b270db95021b8
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-05 11:00:26 +0500

    Add TeacherRepository interface to Desktop

---
+++ Center.Desktop/ServiceLayer/TeacherServie/ITeacherRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ServiceLayer.TeacherServie
+{
+   public  interface ITeacherRepository
+    {
+        Task<IEnumerable<Teacher>> GetAllTeachersAsync();
+        Task<Teacher> GetbyIdTeacherAsync(Guid id);
+        Task<bool> CreateTeacherAsync(Teacher teacher);
+        Task<bool> UpdateTeacherAsync(Teacher teacher);
+        Task<bool> DeleteTeacher(Guid id);
+       // bool ExistTeacher(Guid id);
+    }
+}
