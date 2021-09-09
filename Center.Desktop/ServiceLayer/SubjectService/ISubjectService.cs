f1f1daef5f96be5ad0747e7b9b741b6b23173ba8
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-09 13:53:35 +0500

    add ISubjectService

---
+++ Center.Desktop/ServiceLayer/SubjectService/ISubjectService.cs
@@ -0,0 +1,18 @@
+﻿using Center.API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ServiceLayer.SubjectService
+{
+    interface ISubjectService
+    {
+        Task<IEnumerable<Subject>> GetAllSubjects();
+        Task<Subject> GetByIdSubject(Guid id);
+        Task<string> CreateSubject(SubjectForCreationDto subjectForCreationDto);
+        Task<string> DeleteSubject(Guid id);
+        Task<string> UpdateSubject(Guid id, CustomSubjectDto customSubjectDto);
+    }
+}
