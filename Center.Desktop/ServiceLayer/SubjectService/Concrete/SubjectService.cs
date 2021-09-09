08feff25bf576a0354c7927a1f886961f2da86f8
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-09 13:53:20 +0500

    add SubjectService

---
+++ Center.Desktop/ServiceLayer/SubjectService/Concrete/SubjectService.cs
@@ -0,0 +1,42 @@
+﻿using Center.API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ServiceLayer.SubjectService.Concrete
+{
+    public class SubjectService : ISubjectService
+    {
+        public Task<string> CreateSubject(SubjectForCreationDto subjectForCreationDto)
+        {
+          
+            using (var client=new HttpClient())
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Task<string> DeleteSubject(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Subject>> GetAllSubjects()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Subject> GetByIdSubject(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> UpdateSubject(Guid id, CustomSubjectDto customSubjectDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
