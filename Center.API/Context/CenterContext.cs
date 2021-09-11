a5f16acdf554b51f17d3277a4c65d77efb433259
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-11 12:09:26 +0500

    add set null

--- Center.API/Context/CenterContext.cs
+++ Center.API/Context/CenterContext.cs
@@ -17,18 +17,25 @@ namespace Center.API.Data
         public DbSet<Teacher> Teachers { get; set; }
         public DbSet<Student> Students { get; set; }
         public DbSet<Subject> Subjects { get; set; }
-      
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<StudentGroup>()
-        //        .HasOne(i => i.Student)
-        //        .WithMany(u => u.StudentGroups)
-        //        .HasForeignKey(n => n.StudentId);
-        //    modelBuilder.Entity<StudentGroup>()
-        //       .HasOne(i => i.Group)
-        //       .WithMany(u => u.StudentGroups)
-        //       .HasForeignKey(n => n.GroupId);
-        //}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Group>()
+                .HasOne(p => p.Teacher)
+                .WithMany(u => u.Groups)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Group>()
+               .HasOne(p => p.Subject)
+               .WithMany(u => u.Groups)
+               .OnDelete(DeleteBehavior.SetNull);
+        
+               
+         
+
+
+          
+        }
     }
 }
