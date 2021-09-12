a762137259e42035bddefb60220682e961da631c
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-12 11:43:32 +0500

    add GroupViewModel

---
+++ Center.Desktop/ViewModels/GroupViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Center.Desktop.ServiceLayer.StudentService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ViewModels
+{
+   public  class GroupViewModel
+    {
+       
+        public Guid Id { get; set; }
+                     
+        public string GuruhNomi { get; set; }
+           
+        public string Uqituvchi { get; set; }
+              
+        public string Fan { get; set; }
+        public ICollection<Student> Students { get; set; } = new List<Student>();
+
+
+       
+    }
+}
