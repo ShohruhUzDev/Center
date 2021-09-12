86d5aaf4ad573310a684e4df56475b02818d9ae8
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-12 11:42:19 +0500

    add GroupService to CenterClient

---
+++ Center.Desktop/ServiceLayer/GroupService/IGroupService.cs
@@ -0,0 +1,20 @@
+﻿using Center.API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ServiceLayer.GroupService
+{
+   public  interface IGroupService
+    {
+        Task<IEnumerable<GroupDto>> GetAllGroups();
+        Task<GroupDto> GetByIdGroup(Guid groupid);
+        Task<string> CreateGroup(CreateGroupDto createGroupDto);
+        Task<string> UpdateGroup(Guid id,  UpdateGroupDto updateGroupDto);
+        Task<string> DeleteGroup(Guid groupid);
+        Task<string> AddStudentsToGroup(List<Guid> studentsId, Guid groupId);
+
+    }
+}
