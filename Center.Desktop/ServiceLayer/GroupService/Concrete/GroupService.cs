cd405c9dd2ce9e3e93830a8204e683066e4a7ef4
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-13 16:21:00 +0500

    add new task

--- Center.Desktop/ServiceLayer/GroupService/Concrete/GroupService.cs
+++ Center.Desktop/ServiceLayer/GroupService/Concrete/GroupService.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Center.API.Dtos;
 using Center.API.Profiles;
+using Center.Desktop.ViewModels;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -13,19 +14,34 @@ namespace Center.Desktop.ServiceLayer.GroupService.Concrete
 {
     public class GroupService : IGroupService
     {
-        IMapper _mapper;
-        public GroupService(IMapper mapper)
+        IMapper mapper;
+        public GroupService()
         {
-            _mapper = mapper;
+           
         }
         public Task<string> AddStudentsToGroup(List<Guid> studentsId, Guid groupId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<string> CreateGroup(CreateGroupDto createGroupDto)
+        public async Task<string> CreateGroup(CreateGroupDto createGroupDto)
         {
-            throw new NotImplementedException();
+           using(var client=new HttpClient())
+            {
+                client.BaseAddress = new Uri(GroupAPI.Post_URL);
+
+                var json = JsonConvert.SerializeObject(createGroupDto);
+                StringContent stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+                var res = await client.PostAsync(client.BaseAddress, stringContent);
+
+                if(res.StatusCode==System.Net.HttpStatusCode.Created)
+                {
+                    return await res.Content.ReadAsStringAsync();
+                }
+                return null;
+
+            }
         }
 
         public Task<string> DeleteGroup(Guid groupid)
@@ -33,49 +49,81 @@ namespace Center.Desktop.ServiceLayer.GroupService.Concrete
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<GroupDto>> GetAllGroups()
+        public async Task<IEnumerable<GroupViewModel>> GetAllGroups()
         {
-            using (var client=new HttpClient())
+            using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri(GroupAPI.GetAll_URL);
                 var res = await client.GetAsync(client.BaseAddress);
                 string response = await res.Content.ReadAsStringAsync();
 
-                IEnumerable<GroupDto> groupDtos = _mapper.Map<IEnumerable< GroupDto>>( JsonConvert.DeserializeObject<IEnumerable< Group>>(response));
+                IEnumerable<ReadGroup> groupDtos =JsonConvert.DeserializeObject<IEnumerable<ReadGroup>>(response);
 
-                return groupDtos;
-            }
-        }
-
-        public async Task<GroupDto> GetByIdGroup(Guid groupid)
-        {
-           using (var client=new HttpClient())
-            {
-                client.BaseAddress = new Uri(GroupAPI.Get_URL + $"{groupid}");
-                var res = await client.GetAsync(client.BaseAddress);
-                string response = await res.Content.ReadAsStringAsync();
-                GroupDto groupDto = _mapper.Map<GroupDto>( JsonConvert.DeserializeObject<Group>(response));
-                return groupDto;
-            }
-        }
-
-        public async Task<string> UpdateGroup(Guid id, UpdateGroupDto updateGroupDto)
-        {
-           using (var client=new HttpClient())
-            {
-                client.BaseAddress = new Uri(GroupAPI.Put_URL + $"{id}");
-
-                var sendjson = JsonConvert.SerializeObject(updateGroupDto);
-                StringContent stringContent = new StringContent(sendjson, Encoding.UTF8, "application/json");
-
-                var res = await client.PutAsync(client.BaseAddress, stringContent);
-
-                if(res.StatusCode==System.Net.HttpStatusCode.OK)
+                List<GroupViewModel> groupViewModels = new List<GroupViewModel>();
+                foreach (var i in groupDtos)
                 {
-                    return await res.Content.ReadAsStringAsync();
+                    var students = i.Students;
+                    List<string> stulist = new List<string>();
+                    foreach (var u in students)
+                    {
+                        stulist.Add(u.FirstName + u.LastName);
+                    }
+                    if(stulist is not null)
+                    {
+                        groupViewModels.Add(new GroupViewModel()
+                        {
+                            Id = i.Id,
+                            GuruhNomi = i.GroupName,
+                            Fan = i.Subject?.SubjectName,
+                            Uqituvchi =(i.Teacher is not null)? i.Teacher.FirstName + i.Teacher?.LastName:"Yuq",
+                            Students = stulist
+                        });
+                    }
+                   
+                    
+
+
                 }
-                return null;
+                return groupViewModels;
+                //  return groupDtos;
             }
+
+        }
+
+
+
+            public Task<GroupViewModel> GetByIdGroup(Guid groupid)
+            {
+                throw new NotImplementedException();
+                //using (var client=new HttpClient())
+                // {
+                //     client.BaseAddress = new Uri(GroupAPI.Get_URL + $"{groupid}");
+                //     var res = await client.GetAsync(client.BaseAddress);
+                //     string response = await res.Content.ReadAsStringAsync();
+                //     GroupDto groupDto = mapper.Map<GroupDto>( JsonConvert.DeserializeObject<Group>(response));
+                //     return groupDto;
+                // }
+            }
+
+            public async Task<string> UpdateGroup(Guid id, UpdateGroupDto updateGroupDto)
+            {
+                using (var client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(GroupAPI.Put_URL + $"{id}");
+
+                    var sendjson = JsonConvert.SerializeObject(updateGroupDto);
+                    StringContent stringContent = new StringContent(sendjson, Encoding.UTF8, "application/json");
+
+                    var res = await client.PutAsync(client.BaseAddress, stringContent);
+
+                    if (res.StatusCode == System.Net.HttpStatusCode.OK)
+                    {
+                        return await res.Content.ReadAsStringAsync();
+                    }
+                    return null;
+                }
+            }
+
+
         }
     }
-}
