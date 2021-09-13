1eb0adf7cc973b8b6c1b2739fe33ce67aa843f7d
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-13 16:23:14 +0500

    add new model

---
+++ Center.Desktop/ExternalModels/SubjectForCBX.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ExternalModels
+{
+  public  class SubjectForCBX
+    {
+        public Guid Id { get; set; }
+
+
+        public string Name { get; set; }
+
+
+    }
+}
