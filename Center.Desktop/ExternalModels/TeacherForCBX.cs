9a8cc91e1f8f55cb4213e152fa93f23b831c1984
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-13 16:21:44 +0500

    add new model

---
+++ Center.Desktop/ExternalModels/TeacherForCBX.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ExternalModels
+{
+   public  class TeacherForCBX
+    {
+       
+        public Guid Id { get; set; }
+
+      
+        public string FullName { get; set; }
+
+
+
+   
+    }
+}
