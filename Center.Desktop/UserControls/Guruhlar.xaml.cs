7b3cf6c85404dc0c1b0e99140373eb41066dce7f
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-14 17:04:52 +0500

    Implement Guruhlar xaml

--- Center.Desktop/UserControls/Guruhlar.xaml.cs
+++ Center.Desktop/UserControls/Guruhlar.xaml.cs
@@ -1,7 +1,19 @@
-﻿using Center.Desktop.View;
+﻿using Center.API.Dtos;
+using Center.Desktop.ExternalModels;
+using Center.Desktop.ServiceLayer;
+using Center.Desktop.ServiceLayer.GroupService;
+using Center.Desktop.ServiceLayer.GroupService.Concrete;
+using Center.Desktop.ServiceLayer.SubjectService;
+using Center.Desktop.ServiceLayer.SubjectService.Concrete;
+using Center.Desktop.ServiceLayer.TeacherServie;
+using Center.Desktop.ServiceLayer.TeacherServie.Concrete;
+using Center.Desktop.View;
+using Center.Desktop.ViewModels;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,6 +33,11 @@ namespace Center.Desktop.Pages.UserControls
     /// </summary>
     public partial class Guruhlar : UserControl
     {
+        IGroupService _groupService = new GroupService();
+        ITeacherRepository teacherRepository = new TeacherRepository();
+        ISubjectService subjectService = new SubjectService();
+        IEnumerable<GroupViewModel> groupViewModel = new List<GroupViewModel>();
+
         public Guruhlar()
         {
             InitializeComponent();
@@ -30,6 +47,106 @@ namespace Center.Desktop.Pages.UserControls
         {
             CreateGroupView createGroupView = new CreateGroupView();
             createGroupView.ShowDialog();
+        }
+
+        private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+           
+            groupViewModel = await _groupService.GetAllGroups();
+
+            Guruh_datagrid.ItemsSource = groupViewModel;
+
+
+
+
+            List<TeacherForCBX> teacherForCBX = new List<TeacherForCBX>();
+
+            var teacher = await teacherRepository.GetAllTeachersAsync();
+            foreach (var i in teacher)
+            {
+                teacherForCBX.Add(new TeacherForCBX()
+                {
+                    Id = i.Id,
+                    FullName = i.Name
+
+                });
+            }
+
+
+            List<SubjectForCBX> subjectForCBXes = new List<SubjectForCBX>();
+            var subject = await subjectService.GetAllSubjects();
+
+            foreach (var i in subject)
+            {
+                subjectForCBXes.Add(new SubjectForCBX()
+                {
+                    Id = i.Id,
+                    Name = i.SubjectName
+                });
+            }
+
+
+            Teacherlar_Cbx.ItemsSource = teacherForCBX;
+            Teacherlar_Cbx.DisplayMemberPath = "FullName";
+            Teacherlar_Cbx.SelectedValuePath = "Id";
+
+            Subjectlar_Cbx.ItemsSource = subjectForCBXes;
+            Subjectlar_Cbx.DisplayMemberPath = "Name";
+            Subjectlar_Cbx.SelectedValuePath = "Id";
+
+        
+
+
+        }
+
+        private async void Subjectlar_Cbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            groupViewModel = await _groupService.GetAllGroups();
+
+            var subjectForCBX = (SubjectForCBX) Subjectlar_Cbx.SelectedItem;
+
+           // MessageBox.Show(subjectForCBX.Name);
+             var newgroup=   groupViewModel.Where(i => i.Fan == subjectForCBX.Name);
+
+            Guruh_datagrid.ItemsSource = newgroup;
+
+        }
+
+        private async  void Teacherlar_Cbx_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            groupViewModel = await _groupService.GetAllGroups();
+            
+            var teacherCBX = (TeacherForCBX)Teacherlar_Cbx.SelectedItem;
+
+            var newteacher = groupViewModel.Where(i => i.Uqituvchi == teacherCBX.FullName);
+
+            Guruh_datagrid.ItemsSource = newteacher;
+        }
+
+        private async void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            string groupname = SearchGroup_txt.Text;
+
+            groupViewModel = await _groupService.GetAllGroups();
+
+           // var resultgroup = groupViewModel.Where(i =>EF.Functions.Like(i.GuruhNomi, "%"+ groupname+"%"));
+            var resultgroup = groupViewModel.Where(i =>i.GuruhNomi.ToUpper().StartsWith( groupname.ToUpper()));
+
+            Guruh_datagrid.ItemsSource = resultgroup;
+
+
+
+        }
+
+        private void Delete_btn_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //DataGridRow dataGridRow = sender as DataGridRow ;
+            //var fff= dataGridRow.Item[0]
+            //MessageBox.Show()
         }
     }
 }
