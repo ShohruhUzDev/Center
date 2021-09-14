98b372c050ef57a3cef066294fc0209be8723018
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-14 20:29:32 +0500

    add StudentViewModel

---
+++ Center.Desktop/ViewModels/StudentViewModel.cs
@@ -0,0 +1,30 @@
+﻿using Center.API.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ViewModels
+{
+   public class StudentViewModel
+    {
+
+       
+        public Guid Id { get; set; }
+
+
+        
+
+        public string FullName { get; set; }
+
+
+
+      
+        public string Phone { get; set; }
+
+
+       
+        public ICollection<UpdateGroupDto> Groups { get; set; }
+    }
+}
