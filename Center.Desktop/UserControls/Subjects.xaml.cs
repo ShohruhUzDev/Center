c6357d0522338e9c3c1b7c4b4f7045fd682a2043
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-14 17:05:53 +0500

    add new changes

--- Center.Desktop/UserControls/Subjects.xaml.cs
+++ Center.Desktop/UserControls/Subjects.xaml.cs
@@ -1,4 +1,11 @@
-﻿using Center.Desktop.View;
+﻿using Center.Desktop.ServiceLayer.GroupService;
+using Center.Desktop.ServiceLayer.GroupService.Concrete;
+using Center.Desktop.ServiceLayer.SubjectService;
+using Center.Desktop.ServiceLayer.SubjectService.Concrete;
+using Center.Desktop.ServiceLayer.TeacherServie;
+using Center.Desktop.ServiceLayer.TeacherServie.Concrete;
+using Center.Desktop.View;
+using Center.Desktop.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,6 +28,10 @@ namespace Center.Desktop.UserControls
     /// </summary>
     public partial class Subjects : UserControl
     {
+        IGroupService _groupService = new GroupService();
+        ITeacherRepository teacherRepository = new TeacherRepository();
+        ISubjectService subjectService = new SubjectService();
+        IEnumerable<SubjectViewModel> subjectViewModel = new List<SubjectViewModel>();
         public Subjects()
         {
             InitializeComponent();
@@ -30,6 +41,32 @@ namespace Center.Desktop.UserControls
         {
             CreateSubjectView createSubjectView = new CreateSubjectView();
             createSubjectView.ShowDialog();
+
+
+
+
+        }
+
+        private async void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+           
+            subjectViewModel = await subjectService.GetAllSubjects();
+
+            Subject_datagrid.ItemsSource = subjectViewModel;
+
+        }
+
+        private async void SearchSubject_txt_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string subjectname = SearchSubject_txt.Text;
+
+            subjectViewModel = await subjectService.GetAllSubjects();
+
+            // var resultgroup = groupViewModel.Where(i =>EF.Functions.Like(i.GuruhNomi, "%"+ groupname+"%"));
+            var resultgroup = subjectViewModel.Where(i => i.SubjectName.ToUpper().StartsWith(subjectname.ToUpper()));
+
+            Subject_datagrid.ItemsSource = resultgroup;
+
         }
     }
 }
