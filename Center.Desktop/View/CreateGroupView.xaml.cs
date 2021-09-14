6843c5a6491b61e4c5f54c0a13bb5bc0d3fb07a6
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-14 17:07:08 +0500

    Implement CreateGrouopView

--- Center.Desktop/View/CreateGroupView.xaml.cs
+++ Center.Desktop/View/CreateGroupView.xaml.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿using Center.API.Dtos;
+using Center.Desktop.ExternalModels;
+using Center.Desktop.ServiceLayer.GroupService;
+using Center.Desktop.ServiceLayer.GroupService.Concrete;
+using Center.Desktop.ServiceLayer.SubjectService;
+using Center.Desktop.ServiceLayer.SubjectService.Concrete;
+using Center.Desktop.ServiceLayer.TeacherServie;
+using Center.Desktop.ServiceLayer.TeacherServie.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +27,98 @@ namespace Center.Desktop.View
     /// </summary>
     public partial class CreateGroupView : Window
     {
+        IGroupService groupService = new GroupService();
+        ITeacherRepository teacherRepository = new TeacherRepository();
+        ISubjectService subjectService = new SubjectService();
         public CreateGroupView()
         {
             InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+           List< TeacherForCBX> teacherForCBX = new List<TeacherForCBX>() ;
+            var teacher = await teacherRepository.GetAllTeachersAsync();
+            
+
+            foreach(var i in teacher)
+            {
+                teacherForCBX.Add(new TeacherForCBX()
+                {
+                    Id = i.Id,
+                    FullName = i.Name
+                });
+               
+            }
+
+            List<SubjectForCBX> subjectForCBXes = new List<SubjectForCBX>();
+
+            var subjects = await subjectService.GetAllSubjects();
+
+            foreach (var u in subjects)
+            {
+                subjectForCBXes.Add(new SubjectForCBX()
+                {
+                    Id = u.Id,
+                    Name = u.SubjectName
+                });
+            }
+
+
+            Teacher_cbx.ItemsSource = teacherForCBX;
+            Teacher_cbx.DisplayMemberPath = "FullName";
+            Teacher_cbx.SelectedValuePath = "Id";
+
+            Subject_cbx.ItemsSource = subjectForCBXes;
+            Subject_cbx.DisplayMemberPath = "Name";
+            Subject_cbx.SelectedValuePath = "Id";
+           
+
+        }
+
+        private async void Save_btn_Click(object sender, RoutedEventArgs e)
+        {
+            CreateGroupDto createGroupDto = new CreateGroupDto();
+
+
+            
+             
+            if(Teacher_cbx.SelectedIndex!=-1&&Subject_cbx.SelectedIndex!=-1&& GroupName_txt.Text!="")
+            {
+                Guid teacherid = new Guid();
+
+                bool b = Guid.TryParse(Teacher_cbx.SelectedValue.ToString(), out teacherid);
+
+
+
+                Guid subjectid = new Guid();
+
+                bool c = Guid.TryParse(Subject_cbx.SelectedValue.ToString(), out subjectid);
+
+                createGroupDto.GroupName = GroupName_txt.Text;
+                createGroupDto.TeacherId = teacherid;
+                createGroupDto.SubjectId = subjectid;
+
+            }
+            else
+            {
+                MessageBox.Show("Elementlari tuliq tanlanmadi");
+                GroupName_txt.Clear();
+            }
+            string res= await groupService.CreateGroup(createGroupDto);
+
+            if (res is not null)
+
+            {
+                MessageBox.Show("Guruh yaratildi"); 
+                this.Hide();
+            }
+
+            else
+            {
+                MessageBox.Show("Xatolik yuz berdi");
+            }
+          
         }
     }
 }
