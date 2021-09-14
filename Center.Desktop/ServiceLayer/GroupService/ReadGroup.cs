0ccdd37e068d791295e4c3cebd0e5922f166ac78
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-14 17:01:44 +0500

    add new prop

---
+++ Center.Desktop/ServiceLayer/GroupService/ReadGroup.cs
@@ -0,0 +1,45 @@
+﻿using Center.API.Dtos;
+using Center.API.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ServiceLayer.GroupService
+{
+    class ReadGroup
+    {
+        [JsonProperty("Id")]
+        
+        public Guid Id { get; set; }
+
+
+        [JsonProperty("GroupName")]
+       
+        public string GroupName { get; set; }
+
+
+        [JsonProperty("TeacherId")]
+
+        public Guid? TeacherId { get; set; }
+      
+       
+        [JsonProperty("Teacher")]
+        public ReadTeacherDto? Teacher { get; set; }
+
+        [JsonProperty("subjectId")]
+        public Guid? SubjectId { get; set; }
+
+        [JsonProperty("Subject")]
+        public ReadSubjectDto? Subject { get; set; }
+
+        [JsonProperty("Students")]
+
+        public ICollection<UpdateStudentDto> Students { get; set; }
+
+
+       
+    }
+}
