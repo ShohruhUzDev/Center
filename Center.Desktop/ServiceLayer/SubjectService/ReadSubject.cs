4b368aca26949c842f13008dcb17042b02fed839
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-14 17:02:19 +0500

    add new model

---
+++ Center.Desktop/ServiceLayer/SubjectService/ReadSubject.cs
@@ -0,0 +1,34 @@
+﻿using Center.API.Dtos;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ServiceLayer.SubjectService
+{
+   public class ReadSubject
+    {
+          [JsonProperty("Id")]
+        public Guid Id { get; set; }
+
+
+          [JsonProperty("SubjectName")]
+
+        public string SubjectName { get; set; }
+
+
+
+
+        [JsonProperty("Price")]
+
+        public int Price { get; set; }
+
+
+        [JsonProperty("Groups")]
+
+        public ICollection<UpdateGroupDto> Groups { get; set; }
+       
+    }
+}
