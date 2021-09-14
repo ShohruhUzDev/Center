8eeca76e1e31851ccaa45dbb787ba521c63d38a2
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-14 20:27:41 +0500

    Add StudentService Interfeys

---
+++ Center.Desktop/ServiceLayer/StudentService/IStudentService.cs
@@ -0,0 +1,19 @@
+﻿using Center.API.Dtos;
+using Center.Desktop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Center.Desktop.ServiceLayer.StudentService
+{
+    public  interface IStudentService
+    {
+        Task<IEnumerable<StudentViewModel>> GetAllStudentsAsync();
+        Task<StudentViewModel> GetByIdStudentAsync(Guid studentId);
+        Task<string> CreateStudent(CreateStudentDto createStudentDto);
+        Task<string> DeleteStudent(Guid id);
+        Task<string> UpdateStudent(UpdateStudentDto updateStudentDto);
+    }
+}
