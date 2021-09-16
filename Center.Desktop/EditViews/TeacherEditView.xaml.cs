9b3af2dd12c2f1d023591130481c3a75bc2da2c8
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-16 13:15:09 +0500

    Implement TeacherEdit View

---
+++ Center.Desktop/EditViews/TeacherEditView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Center.Desktop.EditViews
+{
+    /// <summary>
+    /// Interaction logic for TeacherEditView.xaml
+    /// </summary>
+    public partial class TeacherEditView : Window
+    {
+        public TeacherEditView()
+        {
+            InitializeComponent();
+        }
+    }
+}
