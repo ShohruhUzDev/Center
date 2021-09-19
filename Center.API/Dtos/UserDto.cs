ab69737702632b6baa723f988f21c314bdd2a669
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-19 22:35:01 +0500

    Implement UserDto

---
+++ Center.API/Dtos/UserDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Center.API.Dtos
+{
+   public class LoginDto
+    {
+        public string  Email { get; set; }
+        public string  Password { get; set; }
+    }
+    
+    
+    
+    
+    public class UserDto:LoginDto
+    {
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string Role { get; set; }
+    }
+}
